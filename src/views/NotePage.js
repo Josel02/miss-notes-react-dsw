3a29ca3d5e187902b1429662405a0fe86c883f98
Aitor <aitormillantrujillo@gmail.com>
2024-03-20 16:44:31 +0100

    Add axios library and implement note fetching functionality

--- src/views/NotePage.js
+++ src/views/NotePage.js
@@ -1,13 +1,45 @@
 // NoteListPage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NoteForm from '../components/NoteForm';
+import { Alert } from 'react-bootstrap';
 import Layout from '../layouts/Layout';
+import axios from 'axios';
 
 const NoteListPage = () => {
+
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/notes');
+        setNotes(response.data);
+      } catch (error) {
+        console.error('Error fetching notes', error);
+      }
+      finally{
+      setLoading(false);
+      }
+    };
+
+    fetchNotes();
+    }, []);
+
   return (
     <Layout>
       <NoteForm />
       {/* Lista de notas */}
+      {loading ? (
+        <div>Cargando notas...</div>
+      ) : notes.length > 0 ? (
+        notes.map(note => (
+          <div key={note.id}>{note.title}</div>
+        ))
+      ) : (
+        <Alert variant="info">Todavía no hay ninguna nota, ¿Por qué no añades una?</Alert>
+      )
+    } 
     </Layout>
   );
 };
