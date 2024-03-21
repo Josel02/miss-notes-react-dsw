a557d1c34a6053a00b9bb271bb6be2d4d6813abb
Josel02 <Jose.Lara3@alu.uclm.es>
2024-03-21 22:29:06 +0100

    Agregado diseño para volver a contraer la nota y para subir imágnes

--- src/components/addNewNote/NoteForm.js
+++ src/components/addNewNote/NoteForm.js
@@ -4,6 +4,7 @@ import TextInput from './TextInput';
 import ImageAndListModeButtons from './buttons/ImageAndListModeButtons';
 import SaveCancelButtons from './buttons/SaveCancelButtons';
 import '../../styles/NoteForm.css';
+import Masonry from '@mui/lab/Masonry';
 
 const NoteForm = () => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -11,6 +12,7 @@ const NoteForm = () => {
     const [content, setContent] = useState('');
     const [items, setItems] = useState([]);
     const [isList, setIsList] = useState(false);
+    const [images, setImages] = useState([]);
 
     const toggleListMode = () => {
         setIsList(!isList);
@@ -21,47 +23,77 @@ const NoteForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Nota creada:', { title, content, items });
+        console.log('Nota creada:', { title, content, items, images });
         setIsExpanded(false);
         setTitle('');
         setContent('');
         setItems([]);
         setIsList(false);
+        setImages([]);
+    };
+
+    const handleCancel = () => {
+        setIsExpanded(false);
+        setTitle('');
+        setContent('');
+        setItems([]);
+        setIsList(false);
+        setImages([]);
+    };
+
+    const handleImageChange = (e) => {
+        const files = Array.from(e.target.files).map(file => URL.createObjectURL(file));
+        setImages([...images, ...files]);
+    };
+
+    // Ajuste para expandir la nota solo al hacer clic en ciertas áreas
+    const handleExpansionClick = () => {
+        if (!isExpanded) {
+            setIsExpanded(true);
+        }
     };
 
     return (
         <div className="container mt-5">
-            <div className={`card shadow-sm p-3 mb-5 bg-white rounded ${!isExpanded ? "clickable" : ""}`} onClick={() => setIsExpanded(true)}>
+            <div className={`card shadow-sm p-3 mb-5 bg-white rounded ${!isExpanded ? "clickable" : ""}`}>
                 <div className="card-body">
                     {!isExpanded && (
                         <div className="d-flex justify-content-between align-items-center mb-3">
-                            <span className="text-muted">Añade una nota...</span>
+                            {/* Área específica para expandir la nota */}
+                            <span className="text-muted" onClick={handleExpansionClick}>Añade una nota...</span>
                             <div>
-                                <ImageAndListModeButtons toggleListMode={toggleListMode} isList={isList} />
+                                <ImageAndListModeButtons toggleListMode={toggleListMode} isList={isList} handleImageChange={handleImageChange} />
                             </div>
                         </div>
                     )}
                     {isExpanded && (
-                        <>
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-group mb-3">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Título de la nota" value={title} onChange={(e) => setTitle(e.target.value)} required />
-                                </div>
-                                {!isList ? (
-                                    <TextInput content={content} setContent={setContent} />
-                                ) : (
-                                    <ListInput items={items} setItems={setItems} />
-                                )}
-                                <div className="d-flex justify-content-between align-items-center mt-3">
-                                    <div>
-                                        <ImageAndListModeButtons toggleListMode={toggleListMode} isList={isList} />
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group mb-3">
+                                <input type="text" className="form-control form-control-lg" placeholder="Título de la nota" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                            </div>
+                            {!isList ? (
+                                <TextInput content={content} setContent={setContent} />
+                            ) : (
+                                <ListInput items={items} setItems={setItems} />
+                            )}
+
+                            <Masonry columns={{ xs: 2, sm: 3, md: 4 }} spacing={2}>
+                                {images.map((image, index) => (
+                                    <div key={index}>
+                                        <img src={image} alt="preview" style={{ width: '100%', display: 'block' }} />
                                     </div>
-                                    <div>
-                                        <SaveCancelButtons onSave={handleSubmit} onCancel={() => setIsExpanded(false)} />
-                                    </div>
+                                ))}
+                            </Masonry>
+
+                            <div className="d-flex justify-content-between align-items-center mt-3">
+                                <div>
+                                    <ImageAndListModeButtons toggleListMode={toggleListMode} handleImageChange={handleImageChange} />
                                 </div>
-                            </form>
-                        </>
+                                <div>
+                                    <SaveCancelButtons onSave={handleSubmit} onCancel={handleCancel} />
+                                </div>
+                            </div>
+                        </form>
                     )}
                 </div>
             </div>
--- src/components/addNewNote/buttons/ImageAndListModeButtons.js
+++ src/components/addNewNote/buttons/ImageAndListModeButtons.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react'; // Importa useRef desde React
 import { CiSquareCheck, CiImageOn, CiText } from "react-icons/ci";
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
-const ImageAndListModeButtons = ({ toggleListMode, isList }) => {
+const ImageAndListModeButtons = ({ toggleListMode, isList, handleImageChange }) => {
+    const fileInputRef = useRef(null); // Crea una referencia al input de archivo
+
     const renderTooltip = (message) => (<Tooltip>{message}</Tooltip>);
 
     return (
         <>
             <OverlayTrigger placement="top" overlay={renderTooltip("Subir imagen")}>
-                <button className="btn icon-button" onClick={(e) => e.stopPropagation()}>
+                <button className="btn icon-button" onClick={(e) => {
+                    e.stopPropagation();
+                    // Activa el clic en el input de archivo a través de la referencia
+                    fileInputRef.current.click();
+                }}>
                     <CiImageOn size="2em" />
+                    <input 
+                      ref={fileInputRef} // Asigna la referencia aquí
+                      id="image-upload" 
+                      type="file" 
+                      multiple 
+                      style={{ display: 'none' }} 
+                      onChange={handleImageChange} 
+                      accept="image/*" // Nos aseguramos de que solo se puedan subir imágenes
+                    />
                 </button>
             </OverlayTrigger>
+
             <OverlayTrigger placement="top" overlay={renderTooltip(isList ? "Texto" : "Nueva lista")}>
                 <button className="btn icon-button ms-2" onClick={(e) => {
                     e.stopPropagation();
