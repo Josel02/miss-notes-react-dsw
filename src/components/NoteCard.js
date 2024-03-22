4ad8de09275476b516d256088b7df2c1879ea230
Josel02 <Jose.Lara3@alu.uclm.es>
2024-03-22 02:45:43 +0100

    Actualizado vista sin editar de NoteCard para decodificar JSON a texto, listas e imágenes

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,22 +1,56 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, ListGroup } from 'react-bootstrap';
 import '../styles/NoteCard.css';
 
 const NoteCard = ({ note, onEdit }) => {
-    return (
-        <Card className="note-card" style={{ margin: '10px' }}>
-          <Card.Body>
-            <Card.Title>{note.title}</Card.Title>
-            <Card.Text>
-              {note.content}
-            </Card.Text>
-            <div className="action-buttons">
-              <Button variant="primary" onClick={onEdit}>Editar</Button>
-              <Button variant="danger">Eliminar</Button>
-            </div>
-          </Card.Body>
-        </Card>
+  // Asumiendo que note.content podría ser una cadena JSON, intentamos parsearla
+  let content;
+  try {
+    content = typeof note.content === 'string' ? JSON.parse(note.content) : note.content;
+  } catch (error) {
+    console.error('Error parsing note content', error);
+    content = {}; // Si hay un error en el parseo, asumimos contenido vacío
+  }
+
+  const renderContent = (content) => {
+    if (content.items && Array.isArray(content.items)) {
+      return (
+        <ListGroup>
+          {content.items.map((item) => (
+            <ListGroup.Item key={item.id} variant={item.checked ? 'success' : ''}>
+              {item.text}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       );
-    };
-    
-    export default NoteCard;
+    } else if (content.text) {
+      return <p>{content.text}</p>;
+    }
+    return null;
+  };
+
+  const renderImage = (imagePath) => {
+    if (imagePath) {
+      return <img src={imagePath} alt="Nota" className="img-thumbnail" />;
+    }
+    return null;
+  };
+
+  return (
+    <Card className="note-card" style={{ margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>{note.title}</Card.Title>
+        <Card.Text>
+          {renderContent(content)}
+          {content.imagePath ? renderImage(content.imagePath) : null}
+        </Card.Text>
+        <div className="action-buttons">
+          <Button variant="primary" onClick={() => onEdit(note)}>Editar</Button>
+          <Button variant="danger">Eliminar</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default NoteCard;
