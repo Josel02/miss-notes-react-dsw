eb68962d3dc17fe6199174e4072846a9a927ec41
Aitor <aitormillantrujillo@gmail.com>
2024-03-28 12:48:01 +0100

    Add local state and handle checked change in EditContentSection component

--- src/components/EditContentSection.js
+++ src/components/EditContentSection.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Form, ListGroup } from 'react-bootstrap';
 
 const EditContentSection = ({ content, onChange }) => {
+  const [localContent, setLocalContent] = useState(content);
+
+  useEffect(() => {
+    setLocalContent(content); // Actualiza el contenido local cuando el prop `content` cambia
+  }, [content]);
+
   // Manejador para cambios en el texto
   const handleTextChange = (e, index) => {
     // Actualiza el estado de la nota aquí
@@ -10,6 +16,13 @@ const EditContentSection = ({ content, onChange }) => {
   // Manejador para cambios en los elementos de la lista
   const handleListItemChange = (e, index) => {
     // Actualiza el estado de la nota aquí
+  };
+
+  const handleCheckedChange = (item, index, checkedIndex) => {
+    const newContent = [...localContent];
+    newContent[index].data[checkedIndex].checked = !newContent[index].data[checkedIndex].checked;
+    setLocalContent(newContent); 
+    //onChange(newContent); 
   };
 
   // Renderiza la sección de texto
@@ -36,14 +49,30 @@ const EditContentSection = ({ content, onChange }) => {
     </ListGroup>
   );
 
+  const renderCheckedListSection = (list, index) => (
+    <ListGroup>
+      {list.map((item, checkedIdx) =>(
+        <ListGroup.Item key={checkedIdx} className={item.checked ? 'checked' : ''}>
+          <Form.Check 
+            type="checkbox"
+            checked={item.checked}
+            onChange={() => handleCheckedChange(item, index, checkedIdx)}
+          />
+          <span className="item-text">{item.text}</span>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+
   // Renderiza la sección correspondiente basada en el tipo de contenido
   const renderContentByType = (content, index) => {
     switch (content.type) {
       case 'text':
         return renderTextSection(content.data, index);
       case 'list':
-      case 'checked list': // Tratamos las listas y las listas chequeadas de manera similar
         return renderListSection(content.data, index);
+      case 'checked list': 
+        return renderCheckedListSection(content.data, index);
       case 'image':
         return <img src={content.data} alt="Imagen de la nota" />;
       default:
