5f451b367d24d3234b23030c1ad28367a99aeebc
Josel02 <Jose.Lara3@alu.uclm.es>
2024-05-07 19:45:24 +0200

    Primera implementación del FriendList

--- src/views/Friends/FriendList.js
+++ src/views/Friends/FriendList.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const FriendRequests = () => {
-    return <div>Mis Amigos</div>;
+const FriendList = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        const fetchFriends = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await axios.get('http://localhost:3000/friends/listFriends', {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                setFriends(response.data);
+            } catch (error) {
+                console.error('Error al cargar amigos:', error);
+            }
+        };
+
+        fetchFriends();
+    }, []);
+
+    return (
+        <div>
+            <h1>Mis Amigos</h1>
+            <ul>
+                {friends.map(friend => (
+                    <li key={friend._id}>{friend.name} ({friend.email})</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
-export default FriendRequests;
+export default FriendList;
