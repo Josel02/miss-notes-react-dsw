18f9712eff8887c1e69f3de75084b9e304f34441
Josel02 <Jose.Lara3@alu.uclm.es>
2024-05-07 19:14:07 +0200

    Arreglado tamaño vertical del FriendSection

--- src/views/Friends/FriendSection.js
+++ src/views/Friends/FriendSection.js
@@ -6,14 +6,18 @@ import FriendRequests from './FriendRequests';
 import FriendList from './FriendList';
 
 const drawerWidth = 240;
+const navbarHeight = '56px'; // Asumiendo que el navbar tiene una altura de 56px
 
 const FriendSection = () => {
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', marginTop: navbarHeight }}>
             <Drawer
-                style={{ width: drawerWidth }}
+                sx={{ width: drawerWidth, flexShrink: 0, height: `calc(100vh - ${navbarHeight})` }}
                 variant="permanent"
                 anchor="left"
+                PaperProps={{
+                    style: { marginTop: navbarHeight }  // Aplica un margen superior al contenido del Drawer
+                }}
             >
                 <List>
                     <ListItem button component={Link} to="/friends/requests">
@@ -22,14 +26,14 @@ const FriendSection = () => {
                     <ListItem button component={Link} to="/friends/list">
                         <ListItemText primary="Lista de Amigos" />
                     </ListItem>
-                    {/* Añade más ítems según sea necesario */}
+                    {/* Aquí puedes añadir más ítems según sea necesario */}
                 </List>
             </Drawer>
-            <main style={{ flexGrow: 1, padding: '20px' }}>
+            <main style={{ flexGrow: 1, padding: '20px', marginTop: navbarHeight }}>
                 <Routes>
                     <Route path="requests" element={<FriendRequests />} />
                     <Route path="list" element={<FriendList />} />
-                    {/* Añade más rutas según sea necesario */}
+                    {/* Añade más rutas internas según sea necesario */}
                 </Routes>
             </main>
         </div>
