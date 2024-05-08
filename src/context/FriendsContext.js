4f737a76a58932eb9c322031c299b935c3eb8d1c
Aitor <aitormillantrujillo@gmail.com>
2024-05-08 21:28:38 +0200

    feat: Update FriendCard component to use "Remove friend" instead of "Delete"
    The commit modifies the FriendCard component to update the label of the button for removing a friend. The text is changed from "Delete" to "Remove friend" to improve clarity and provide a more user-friendly experience.

--- src/context/FriendsContext.js
+++ src/context/FriendsContext.js
@@ -51,3 +51,15 @@ export const acceptFriendRequest = async (friendshipId, token, enqueueSnackbar) 
     throw error;
   }
 };
+
+export const removeFriend = async (friendshipId, token, enqueueSnackbar) => {
+  try {
+    await axios.delete(`${apiUrl}/deleteFriendship/${friendshipId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    enqueueSnackbar('Friend removed successfully.', { variant: 'success' });
+  } catch (error) {
+    console.error('API error:', error);
+    throw error;
+  }
+}
--- src/views/Friends/FriendList.js
+++ src/views/Friends/FriendList.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Masonry from '@mui/lab/Masonry'; // Import Masonry from MUI
-import FriendCard from '../../components/FriendCard'; // Make sure the path is correct
+import Masonry from '@mui/lab/Masonry';
+import { useSnackbar } from 'notistack';
+import FriendCard from '../../components/FriendCard';
+import { removeFriend } from '../../context/FriendsContext';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../components/AuthContext';
 
 const FriendList = () => {
     const [friends, setFriends] = useState([]);
+    const { logout } = useAuth();
+    const { enqueueSnackbar } = useSnackbar();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchFriends = async () => {
@@ -20,16 +27,55 @@ const FriendList = () => {
         };
 
         fetchFriends();
+        console.log(friends);
     }, []);
 
+    const onRemove = async (friendshipId) => {
+        try {
+            const token = localStorage.getItem('token');
+            await removeFriend(friendshipId, token, enqueueSnackbar);
+            setFriends(friends.filter(friend => friend._id !== friendshipId));
+        } catch (error) {
+            handleAPIError(error);
+        }
+    }
+
+    const handleAPIError = (error) => {
+        console.error('API error:', error);
+        if (error.response && error.response.status === 403) {
+          navigate('/', { replace: true });
+          enqueueSnackbar('Session expired. Please login again.', { variant: 'warning' });
+          logout();
+        } 
+        else if (error.response && error.response.status === 404) {
+          enqueueSnackbar(error.response.data.message, { variant: 'info' });
+        }
+        else if (error.response && error.response.status === 400) {
+          enqueueSnackbar("You have already sent a friend request to this user", { variant: 'info' });
+        }
+        else {
+          enqueueSnackbar('Error processing the request.', { variant: 'error' });
+        }
+      };
+
     return (
+        <>
+        <h2>My friends</h2>
         <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
             {friends.map(friend => (
                 <div key={friend._id}>
-                    <FriendCard name={friend.name} email={friend.email} adding={false} />
+                    <FriendCard 
+                        name={friend.name}
+                        email={friend.email}
+                        onClick={() => onRemove(friend._id)}
+                        status={"friend"}
+                    />
                 </div>
             ))}
         </Masonry>
+        <hr/>
+        <h2>Your requests</h2>
+        </>
     );
 };
 
